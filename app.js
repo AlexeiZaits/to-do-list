39ffbb14a6f5ba2fb2e0f4115ab1377cf01ad2a7
Алексей <zaykuh@gmail.com>
2024-01-05 00:25:09 +0500

    refactor: refactor for app.js

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 
+//Globals
+todo_list = document.getElementById('todo-list')
+user_todo = document.getElementById('user-todo')
+let authors = [];
+let todos = [];
 
+//Attach Events
+document.addEventListener('DOMContentLoaded', initApp)
 add_todo_button = document.getElementById('add_todo')
 .addEventListener('click', add_todo)
 
-handle_request('https://jsonplaceholder.typicode.com/users', 'GET')
-.then(data => {
-    construct_list_users(data)
-    
-})
-.catch(error => {
-    console.log(`Ошибка при получение данных ${error}`)
-})
+function initApp(){
+    Promise.all([get_data('users', 'GET'),get_data('todos', 'GET')]).then(values => {
+        [authors,todos] = values
+        authors.forEach(author => print_author(author));
+        todos.forEach(todo => print_todo(todo, 'beforeend'))
+    })
 
-// отправление запросов на сервер
+}
+
+// Async logic
+async function get_data(link, request){
+    data = handle_request(`https://jsonplaceholder.typicode.com/${link}`, request)
+    .then(data => {
+        return JSON.parse(data)
+    })
+    .catch(error => {
+        console.log(`Ошибка при получение данных ${error}`)
+    })
+    return data
+}
+
 async function handle_request(link, request, json=undefined){
 
         return fetch(link, {
@@ -22,8 +40,6 @@ async function handle_request(link, request, json=undefined){
         })
         .then(response =>{
             if (response.ok){
-               
-               // return response.json()
                return response.text()
             }
             else{
@@ -36,50 +52,30 @@ async function handle_request(link, request, json=undefined){
         })
 }
 
-function construct_list_users(users){
-    users = JSON.parse(users)
-    user_todo = document.getElementById('user-todo')
-    for (let user of users){
-        user_todo.insertAdjacentHTML('beforeend', `
-            <option data-id="${user['id']}">${user['name']}</option>
-        `)
-
-    }
-
-    get_todos(users)
+function print_author(user){
+    user_todo.insertAdjacentHTML('beforeend', `
+        <option data-id="${user['id']}">${user['name']}</option>
+    `)
     
 }
 
-async function get_todos(users){
-
-    handle_request('https://jsonplaceholder.typicode.com/todos', 'GET')
-    .then(data => {
-        data = JSON.parse(data)
-        construct_todos(data, users)
-    })
-    .catch(error => {
-        console.log(`Ошибка при получение данных ${error}`)
-    })
+function print_todo(post, position){
+    author = find_author(post)
+    todo_list.insertAdjacentHTML(position, `
+    <li class="todo-item">
+    <input type="checkbox" ${post['completed'] === true ? 'checked' : ''} onclick="change_status(event)">
+    <p class="todo" data-id=${post['id']}>${post['title']}<br><strong>${author}</strong></p>
+    <p class="close" onclick="delete_todo(event)">&times;</p>
+    </li>
+    `)
     
 }
 
-// констркуктор todos
-function construct_todos(posts, users){
-    todo_list = document.getElementById('todo-list')
-    todo_list.innerHTML = '';
-    for (let post of posts){
-        author = users.find(item => item.id === post.userId).name
-        todo_list.insertAdjacentHTML('beforeend', `
-        <li class="todo-item">
-        <input type="checkbox" ${post['completed'] === true ? 'checked' : ''} onclick="change_status(event)">
-        <p class="todo" data-id=${post['id']}>${post['title']}<br><strong>${author}</strong></p>
-        <p class="close" onclick="delete_todo(event)">&times;</p>
-        </li>
-        `)
-    }
+function find_author(post){
+    author = authors.find(item => item.id === post.userId).name
+    return author
 }
 
-//изменение статуса
 function change_status(event){
     todoId = event.target.parentElement
     .querySelector('.todo').dataset.id
@@ -87,7 +83,7 @@ function change_status(event){
     
 }
 
-//удаление todo
+
 function delete_todo(event){
     todo = event.target.parentElement;
     todoId = todo.querySelector('.todo').dataset.id;
@@ -101,31 +97,24 @@ function delete_todo(event){
         console.log(`Ошибка: ${error}`)
     })
     
-    
 }
 
-//добавление todo
 function add_todo(event){
     event.preventDefault();
-    json = {}
+    
     if(document.getElementById('user-todo').value !== 'select user'){
-        author = document.getElementById('user-todo').value;
+        author_id = user_todo.value;
+        author_id = authors.find(user => user.name === user_todo.value).id
+        
         new_todo = document.getElementById('new-todo').value;
-        console.log(document.getElementById('user-todo').dataset.id)
-        json['author'] = author
-        json['title'] = new_todo
-        console.log(json)
+        json = { userId: author_id, title: new_todo, completed: false, };
         handle_request(`https://jsonplaceholder.typicode.com/todos`, 'POST', json)
         .then(data => {
             if (data){
-                data = {id: 200} // имитация работы с базой данных
-                document.getElementById('todo-list').insertAdjacentHTML('afterbegin', `
-                <li class="todo-item">
-                <input type="checkbox" onclick="change_status(event)">
-                <p class="todo" data-id=${data.id}>${new_todo}<br><strong>${author}</strong></p>
-                <p class="close" onclick="delete_todo(event)">&times;</p>
-                </li>
-                `)
+                data = {id: 200}
+                json['id'] = data.id // имитация работы с базой данных получение id нового поста
+                print_todo(json, 'afterbegin')
+                
             }
         })
         .catch(error =>{
@@ -134,10 +123,8 @@ function add_todo(event){
 
     }
     else{
-        alert('Выбирите,пожалуйста, пользователя')
+        alert('Выбирите, пожалуйста, пользователя')
     }
-    document.getElementById('new-todo').value;
-    document.getElementById('user-todo').value;
 }
 
 
